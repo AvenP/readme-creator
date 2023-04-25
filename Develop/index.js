eccbf75c3c832eb34b5f72fead90fd43fd9b5fce
aboyd47 <123212035+aboyd47@users.noreply.github.com>
2023-04-24 23:12:09 -0500

    done with task

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  
+  {
+    type: "input",
+    name: "creator",
+    message: "What is your GitHub username?",
+  },
+ 
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name your Project?",
+  },
+  
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "MPL2.0", "none"],
+  },
+
+  {
+    type: "input",
+    name: "description",
+    message: "Please give a short summary of your project.",
+  },
+ 
+  {
+    type: "input",
+    name: "link",
+    message: "What is the URL of your live page?"
+  },
+  
+  {
+    type: "input",
+    name: "features",
+    message: "what are some features about this project?",
+  },
+  
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "State the languages or technologies associated with this project.",
+  },
+ 
+ 
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("create README File...");
+        writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+    });
+}
 
 // Function call to initialize app
 init();
